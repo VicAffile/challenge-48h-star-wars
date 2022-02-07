76dcb0c83817488972228117e703608232fab0d4
LBROCHARD <73283488+LBROCHARD@users.noreply.github.com>
2022-02-07 16:01:02 +0100

    added films handeling in /peoples/id in server.js

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const swig = require('swig');
 const path = require('path');
 const bodyParser = require('body-parser');
+const axios = require('axios');
+
+const swapi = "https://swapi.dev/api/";
 
 let app = express();
 app.use(bodyParser());
@@ -21,15 +24,82 @@ app.get('/', (req, res) => {
     res.render("index", data);
 });
 
+app.get('/films', (req, res) => {
+    const data = { title: "films" };
+    res.render("index", data);
+});
+
+app.get('/films/:id', async(req, res) => {
+    const id = parseInt(req.params.id);
+
+    let api = await request(swapi + "films/" + id);
+
+    let characters = [];
+    for (let index in api.characters) {
+        const url = api.characters[index];
+        const number = url.split("/")[url.split("/").length - 2];
+        let name = await request(url);
+        name = name.name;
+        characters.push({ name: name, url: "/people/" + number });
+    };
+    api.characters = characters;
+    console.log(characters);
+
+    const data = { title: api.title };
+    res.render("index", data);
+});
+
+app.get('/people', (req, res) => {
+    const data = { title: "people" };
+    res.render("index", data);
+});
+
+app.get('/people/:id', async(req, res) => {
+    const id = parseInt(req.params.id);
+
+    let api = await request(swapi + "people/" + id);
+
+    let films = [];
+    for (let index in api.films) {
+        const url = api.films[index];
+        const number = url.split("/")[url.split("/").length - 2];
+        let film = await request(url);
+        film = film.title;
+        films.push({ film: film, url: "/people/" + number });
+    };
+    api.films = films;
+    console.log(films);
+
+    const data = { title: api.name };
+    res.render("index", data);
+});
+
+app.get('/planets', (req, res) => {
+    const data = { title: "planets" };
+    res.render("index", data);
+
+});
+
+app.get('/planets/:id', async(req, res) => {
+    const id = parseInt(req.params.id);
+
+    let api = await request(swapi + "planets/" + id);
+
+    const data = { title: api.name };
+    res.render("index", data);
+});
+
 app.get('/species', (req, res) => {
     const data = { title: "Species" };
     res.render("index", data);
 });
 
-app.get('/species/:id', (req, res) => {
+app.get('/species/:id', async(req, res) => {
     const id = parseInt(req.params.id);
-    const data = { title: "Species" };
-    console.log(id);
+
+    let api = await request(swapi + "species/" + id);
+
+    const data = { title: api.name };
     res.render("index", data);
 });
 
@@ -38,9 +108,12 @@ app.get('/starships', (req, res) => {
     res.render("index", data);
 });
 
-app.get('/starships/:id', (req, res) => {
+app.get('/starships/:id', async(req, res) => {
     const id = parseInt(req.params.id);
-    const data = { title: "Starships" };
+
+    let api = await request(swapi + "starships/" + id);
+
+    const data = { title: api.name };
     res.render("index", data);
 });
 
@@ -49,11 +122,24 @@ app.get('/vehicles', (req, res) => {
     res.render("index", data);
 });
 
-app.get('/vehicles/:id', (req, res) => {
+app.get('/vehicles/:id', async(req, res) => {
     const id = parseInt(req.params.id);
-    const data = { title: "Vehicles" };
+
+    let api = await request(swapi + "vehicles/" + id);
+
+    const data = { title: api.name };
     res.render("index", data);
 });
 
 app.listen(process.env.PORT || 8080);
 console.log("L'application tourne.");
+
+async function request(pathApi) {
+    try {
+        const response = await axios.get(pathApi);
+        //console.log(response);
+        return response.data;
+    } catch (error) {
+        console.error(error);
+    }
+}
