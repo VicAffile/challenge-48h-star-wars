995c8119cff3743e63a5810f6d5ee54ffc1b9a67
VicAffile <victoraffile@gmail.com>
2022-02-09 07:06:35 +0100

    Quizz

--- publics/js/quizz.js
+++ publics/js/quizz.js
@@ -1,36 +1,63 @@
-const axios = require('axios');
-
-const swapi = "https://swapi.dev/api/";
-
-async function request(pathApi) {
-    // async
-    try {
-        // await
-        const response = await axios.get(pathApi);
-        // console.log(response);
-        return response.data;
-    } catch (error) {
-        console.error(error);
-    }
+if (localStorage.getItem('challenge-48h-star-wars-groupe-2') == undefined) {
+    localStorage.setItem('challenge-48h-star-wars-groupe-2', JSON.stringify([{
+            name_quizz: "general",
+            "score": 0,
+            "question": 0
+        },
+        {
+            name_quizz: "people",
+            "score": 0,
+            "question": 0
+        },
+        {
+            name_quizz: "film",
+            "score": 0,
+            "question": 0
+        },
+        {
+            name_quizz: "planet",
+            "score": 0,
+            "question": 0
+        }
+    ]));
 }
 
-async function quizz(){
-    let awnserObjects = [];
-    let goodAwnser = none;
-    let maxHeight = 0;
-    let id = 1;
-
-    for (let i = 0; i < 4; i++){
-        let people = await request(swapi + "people/" + id + "/");
-        if (people.height > maxHeight) {
-            maxHeight = guy.height;
+function send() {
+    const a = document.getElementById('a');
+    const b = document.getElementById('b');
+    const c = document.getElementById('c');
+    const d = document.getElementById('d');
+    if (a.checked || b.checked || c.checked || d.checked) {
+        const good = document.getElementById('form').dataset.good;
+        const url = document.location.href;
+        const type = url.split('/')[url.split('/').length - 1];
+        let lastQuizzs = JSON.parse(localStorage.getItem('challenge-48h-star-wars-groupe-2'));
+        let newQuizzs = [];
+        let nbrQuestions;
+        if ((a.checked && a.value == good) || (b.checked && b.value == good) || (c.checked && c.value == good) || (d.checked && d.value == good)) {
+            for (let quizz of lastQuizzs) {
+                if (type == quizz.name_quizz) {
+                    quizz.score++;
+                    quizz.question++;
+                    nbrQuestions = quizz.question;
+                }
+                newQuizzs.push(quizz);
+            }
+        } else {
+            for (let quizz of lastQuizzs) {
+                if (type == quizz.name_quizz) {
+                    quizz.question++;
+                    nbrQuestions = quizz.question;
+                }
+                newQuizzs.push(quizz);
+            }
         }
-        awnserObjects.push({ name: people.name, height: people.height });
+        localStorage.removeItem('challenge-48h-star-wars-groupe-2');
+        localStorage.setItem('challenge-48h-star-wars-groupe-2', JSON.stringify(newQuizzs));
+        if (nbrQuestions < 10) {
+            window.location.href = "/quizz/" + type;
+        } else {
+            window.location.href = "/quizz/result";
+        }
     }
-
-    // awnserObjects.push({ name: 'Luke Skywalker', height: 172 });
-    // awnserObjects.push({ name: 'perso 2', height: 160 });
-    // awnserObjects.push({ name: 'perso 3', height: 150 });
-    // awnserObjects.push({ name: 'perso 4', height: 140 });
-
 }
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const axios = require('axios');
 
 const questions = require('./json/questions.json');
+const { is } = require('express/lib/request');
 
 const swapi = "https://swapi.dev/api/";
 
@@ -22,6 +23,54 @@ let app = express();
 app.use(bodyParser());
 app.use(express.static(path.join(__dirname, 'publics')));
 app.use(express.json());
+
+// J'utilise ces arrays pour gagner du temps de requete, je met en dessous en commentaire le code qui m'a permit de l'obtenir et que j'utiliserai pour obtenir le contenu
+const idPeople = [
+    '1', '2', '3', '4', '5', '6', '7', '8', '9',
+    '10', '11', '12', '13', '14', '15', '16', '18', '19',
+    '20', '21', '22', '23', '24', '25', '26', '27', '28',
+    '29', '30', '31', '32', '33', '34', '35', '36', '37',
+    '38', '39', '40', '41', '42', '43', '44', '45', '46',
+    '47', '48', '49', '50', '51', '52', '53', '54', '55',
+    '56', '57', '58', '59', '60', '61', '62', '63', '64',
+    '65', '66', '67', '68', '69', '70', '71', '72', '73',
+    '74', '75', '76', '77', '78', '79', '80', '81'
+];
+/*
+let characters = [];
+let page = await request("https://swapi.dev/api/people");
+while (page.next != null) {
+    for (let index in page.results) {
+        const url = page.results[index].url
+        const number = url.split("/")[url.split("/").length - 2];
+        characters.push(number);
+    }
+    page = await request(page.next);
+}
+*/
+const idPlanets = [
+    '1', '2', '3', '4', '5', '6', '7',
+    '8', '9', '10', '11', '12', '13', '14',
+    '15', '16', '17', '18', '19', '20', '21',
+    '22', '23', '24', '25', '26', '27', '28',
+    '29', '30', '31', '32', '33', '34', '35',
+    '36', '37', '38', '39', '40', '41', '42',
+    '43', '44', '45', '46', '47', '48', '49',
+    '50'
+];
+/*
+let planets = [];
+let page = await request("https://swapi.dev/api/planets");
+while (page.next != null) {
+    for (let index in page.results) {
+        const url = page.results[index].url
+        const number = url.split("/")[url.split("/").length - 2];
+        planets.push(number);
+    }
+    page = await request(page.next);
+}
+ */
+
 
 
 app.engine('html', swig.renderFile);
@@ -60,8 +109,8 @@ app.get('/films/:id', async(req, res) => {
     for (let index in api.characters) {
         const url = api.characters[index];
         const number = url.split("/")[url.split("/").length - 2];
-        let name = await request(url);
-        name = name.name;
+        const character = await request(url);
+        const name = character.name;
         characters.push({ name: name, url: "/people/" + number });
     };
     api.characters = characters;
@@ -70,8 +119,8 @@ app.get('/films/:id', async(req, res) => {
     for (let index in api.planets) {
         const url = api.planets[index];
         const number = url.split("/")[url.split("/").length - 2];
-        let name = await request(url);
-        name = name.name;
+        const planet = await request(url);
+        const name = planet.name;
         planets.push({ name: name, url: "/planets/" + number });
     };
     api.planets = planets;
@@ -80,8 +129,8 @@ app.get('/films/:id', async(req, res) => {
     for (let index in api.species) {
         const url = api.species[index];
         const number = url.split("/")[url.split("/").length - 2];
-        let name = await request(url);
-        name = name.name;
+        const specie = await request(url);
+        const name = specie.name;
         species.push({ name: name, url: "/species/" + number });
     };
     api.species = species;
@@ -90,8 +139,8 @@ app.get('/films/:id', async(req, res) => {
     for (let index in api.starships) {
         const url = api.starships[index];
         const number = url.split("/")[url.split("/").length - 2];
-        let name = await request(url);
-        name = name.name;
+        const starship = await request(url);
+        const name = starship.name;
         starships.push({ name: name, url: "/starships/" + number });
     };
     api.starships = starships;
@@ -100,8 +149,8 @@ app.get('/films/:id', async(req, res) => {
     for (let index in api.vehicles) {
         const url = api.vehicles[index];
         const number = url.split("/")[url.split("/").length - 2];
-        let name = await request(url);
-        name = name.name;
+        const vehicle = await request(url);
+        const name = vehicle.name;
         vehicles.push({ name: name, url: "/vehicles/" + number });
     };
     api.vehicles = vehicles;
@@ -283,7 +332,7 @@ app.get('/planets/:id', async(req, res) => {
         const number = url.split("/")[url.split("/").length - 2];
         let name = await request(url);
         name = name.name;
-        residents.push({ name: name, url: "/residents/" + number });
+        residents.push({ name: name, url: "/people/" + number });
     };
     api.residents = residents;
 
@@ -402,13 +451,42 @@ app.get('/quizz', (req, res) => {
     res.render("listQuizz", data);
 });
 
-app.get('/quizz/general', (req, res) => {
+app.get('/quizz/result', (req, res) => {
+    const data = { title: "Result", navbar: navbar };
+    res.render("result", data);
+});
+
+app.get('/quizz/general', async(req, res) => {
     const questionNumber = questions.length
     const number = getRandomIntInclusive(1, questionNumber);
-    const question = selectQuestion(number);
-    const data = { title: "Choix du quizz", navbar: navbar };
+    const question = await selectQuestion("general", number);
+    const data = { title: "Quizz Général", navbar: navbar, question: question };
     res.render("quizz", data);
-})
+});
+
+app.get('/quizz/people', async(req, res) => {
+    const questionNumber = questions.length
+    const number = getRandomIntInclusive(1, questionNumber);
+    const question = await selectQuestion("people", number);
+    const data = { title: "Quizz Général", navbar: navbar, question: question };
+    res.render("quizz", data);
+});
+
+app.get('/quizz/film', async(req, res) => {
+    const questionNumber = questions.length
+    const number = getRandomIntInclusive(1, questionNumber);
+    const question = await selectQuestion("film", number);
+    const data = { title: "Quizz Général", navbar: navbar, question: question };
+    res.render("quizz", data);
+});
+
+app.get('/quizz/planet', async(req, res) => {
+    const questionNumber = questions.length
+    const number = getRandomIntInclusive(1, questionNumber);
+    const question = await selectQuestion("planet", number);
+    const data = { title: "Quizz Général", navbar: navbar, question: question };
+    res.render("quizz", data);
+});
 
 app.listen(process.env.PORT || 8080);
 
@@ -433,23 +511,147 @@ function buttons(api, page) {
     return buttons;
 }
 
-function selectQuestion(number) {
+async function selectQuestion(category, number) {
     const data = questions.find(data => data.id === number);
-    console.log(data);
-    let question = { question: null, response: { a: null, b: null, c: null, d: null }, good: null };
+    let question = { question: null, responses: { a: null, b: null, c: null, d: null }, good: null };
+    let responses = [];
+    let characterIndex;
+    let filmIndex;
+    let planetIndex;
+    let character;
+    let film;
+    let planet;
     switch (data.id) {
         case 1:
+            if (category == "general" || category == data.category) {
+                characterIndex = getRandomIntInclusive(0, idPeople.length - 1);
+                character = await request("https://swapi.dev/api/people/" + idPeople[characterIndex]);
+                responses.push(character.homeworld.split("/")[character.homeworld.split("/").length - 2]);
+                while (responses.length < 4) {
+                    let index = getRandomIntInclusive(0, idPlanets.length - 1);
+                    if (!responses.includes(idPlanets[index])) {
+                        responses.push(idPlanets[index]);
+                    }
+                }
+            }
+            responses.sort();
+            question = {
+                question: character.name + data.question,
+                responses: {
+                    a: (await request("https://swapi.dev/api/planets/" + responses[0])).name,
+                    b: (await request("https://swapi.dev/api/planets/" + responses[1])).name,
+                    c: (await request("https://swapi.dev/api/planets/" + responses[2])).name,
+                    d: (await request("https://swapi.dev/api/planets/" + responses[3])).name
+                },
+                good: (await request("https://swapi.dev/api/planets/" + character.homeworld.split("/")[character.homeworld.split("/").length - 2])).name
+            };
             break;
         case 2:
+            if (category == "general" || category == data.category) {
+                characterIndex = getRandomIntInclusive(0, idPeople.length - 1);
+                character = await request("https://swapi.dev/api/people/" + idPeople[characterIndex]);
+                responses.push(character.mass);
+                while (responses.length < 4) {
+                    let mass = getRandomIntInclusive(parseInt(character.mass) - 5, parseInt(character.mass) + 5);
+                    if (!responses.includes(mass)) {
+                        responses.push(mass);
+                    }
+                }
+                responses.sort();
+                question = {
+                    question: character.name + data.question,
+                    responses: {
+                        a: responses[0],
+                        b: responses[1],
+                        c: responses[2],
+                        d: responses[3]
+                    },
+                    good: character.mass
+                };
+            }
             break;
         case 3:
+            if (category == "general" || category == data.category) {
+                characterIndex = getRandomIntInclusive(0, idPeople.length - 1);
+                character = await request("https://swapi.dev/api/people/" + idPeople[characterIndex]);
+                responses.push(character.height);
+                while (responses.length < 4) {
+                    let height = getRandomIntInclusive(parseInt(character.height) - 5, parseInt(character.height) + 5);
+                    if (!responses.includes(height)) {
+                        responses.push(height);
+                    }
+                }
+                responses.sort();
+                question = {
+                    question: character.name + data.question,
+                    responses: {
+                        a: responses[0],
+                        b: responses[1],
+                        c: responses[2],
+                        d: responses[3]
+                    },
+                    good: character.height
+                };
+            }
             break;
+        case 4:
+            if (category == "general" || category == data.category) {
+                filmIndex = getRandomIntInclusive(1, 6);
+                film = await request("https://swapi.dev/api/films/" + filmIndex);
+                responses.push(film.episode_id);
+                while (responses.length < 4) {
+                    let episode_id = getRandomIntInclusive(1, 6);
+                    if (!responses.includes(episode_id)) {
+                        responses.push(episode_id);
+                    }
+                }
+                responses.sort();
+                question = {
+                    question: film.title + data.question,
+                    responses: {
+                        a: responses[0],
+                        b: responses[1],
+                        c: responses[2],
+                        d: responses[3]
+                    },
+                    good: film.episode_id
+                };
+                console.log(question)
+            }
+            break;
+        case 5:
+            if (category == "general" || category == data.category) {
+                planetIndex = getRandomIntInclusive(0, idPlanets.length - 1);
+                planet = await request("https://swapi.dev/api/planets/" + idPlanets[planetIndex]);
+                responses.push(planet.population);
+                while (responses.length < 4) {
+                    let index = getRandomIntInclusive(0, idPlanets.length - 1);
+                    if (!responses.includes(idPlanets[index])) {
+                        responses.push(idPlanets[index]);
+                    }
+                }
+                console.log(responses)
+                responses.sort();
+                question = {
+                    question: planet.name + data.question,
+                    responses: {
+                        a: responses[0],
+                        b: responses[1],
+                        c: responses[2],
+                        d: responses[3]
+                    },
+                    good: planet.population
+                };
+                console.log(question)
+            }
+            break;
+        default:
+            return question = selectQuestion(category, number);
     }
     return question;
 }
 
 function getRandomIntInclusive(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+    const number = Math.floor(Math.random() * (max - min + 1)) + min;
+    return number;
 }
